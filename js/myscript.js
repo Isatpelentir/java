8804e6e27a14032a80e4cb8a0fa6386f5b26ef72
Isatpelentir <142498715+Isatpelentir@users.noreply.github.com>
2023-10-20 17:53:52 +0000

    não aguento mais!

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+function trocarFrase(){
+    var fraseAtual=document.getElementById("frase");
+    fraseAtual.innerHTML= "Qualquer frase!";
+    fraseAtual.style.color= "lightblue";
+    fraseAtual.style.fontSize= "25px";
+    fraseAtual.style.backgroundColor= "lightpink";
+    document.body.style.backgroundColor= "purple";
+    fraseAtual.innerHTML= "<img src=\'img/miamiheat.png\'/>";
+}
+
+function trocarClasse(){
+    var fraseAtual=document.getElementById("frase");
+    fraseAtual.className="h1_2";
+}
